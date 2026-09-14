2253573c06566a762642ba03445b6c52f8e9eb4d
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-14 16:33:30 +0100

    style: format plan selection component

--- app/Livewire/PlanSelection.php
+++ app/Livewire/PlanSelection.php
@@ -27,7 +27,7 @@ class PlanSelection extends Component
     public function render()
     {
         return view('livewire.plan-selection', [
-            'plans' => Plan::all()
+            'plans' => Plan::all(),
         ])->layout('layouts.app');
     }
 }
