27d93143df245d4dad2e3a6aa71184703f239048
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-14 17:30:43 +0100

    fix: keep landing layout accessibility markup valid

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -24,7 +24,6 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-white text-zinc-950 antialiased dark:bg-zinc-950 dark:text-white">
-    <a href="#main-content" class="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[100] focus:rounded-lg focus:bg-zinc-950 focus:px-4 focus:py-2 focus:text-sm focus:font-semibold focus:text-white">Saltar para o conteúdo</a>
     {{ $slot }}
     @fluxScripts
 </body>
