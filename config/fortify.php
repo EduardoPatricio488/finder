80dead143912b218bcaf04d72877c104d9ad7bc3
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-15 19:27:49 +0100

    Use entry as login landing path

--- config/fortify.php
+++ config/fortify.php
@@ -3,40 +3,27 @@
 use Laravel\Fortify\Features;
 
 return [
-
     'guard' => 'web',
-
     'passwords' => 'users',
-
     'username' => 'email',
-
     'email' => 'email',
-
     'lowercase_usernames' => true,
-
-    'home' => '/dashboard',
-
+    'home' => '/entrada',
     'prefix' => '',
-
     'domain' => null,
-
     'middleware' => ['web'],
-
     'limiters' => [
         'login' => 'login',
         'two-factor' => 'two-factor',
         'passkeys' => 'passkeys',
     ],
-
     'views' => true,
-
     'passkeys' => [
         'relying_party_id' => parse_url(config('app.url'), PHP_URL_HOST),
         'allowed_origins' => [config('app.url')],
         'user_handle_secret' => env('PASSKEYS_USER_HANDLE_SECRET', config('app.key')),
         'timeout' => 60000,
     ],
-
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
@@ -49,5 +36,4 @@ return [
             'confirmPassword' => true,
         ]),
     ],
-
 ];
