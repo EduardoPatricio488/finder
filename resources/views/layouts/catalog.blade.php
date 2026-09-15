c4566c49b089b1a5523978a57e4a3561c237a9d9
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-15 19:30:48 +0100

    Add direct catalogue navigation to sales introduction

--- resources/views/layouts/catalog.blade.php
+++ resources/views/layouts/catalog.blade.php
@@ -18,6 +18,7 @@
             @if (request()->routeIs('sales'))
                 <div class="mx-auto max-w-7xl px-6 pt-6 text-sm font-semibold text-stone-500 lg:px-10">
                     <span>Coisas bonitas para viver melhor.</span>
+                    <a href="{{ route('products') }}" class="ml-4 underline underline-offset-4 hover:text-stone-950">Ver produtos</a>
                     @auth
                         <span class="ml-3">Olá, {{ auth()->user()->name }}</span>
                     @endauth
