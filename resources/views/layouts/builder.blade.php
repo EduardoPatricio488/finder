7f3c2e64c5e283677f171d93cbdf79a53ba7a679
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-16 11:54:52 +0100

    Add Finder builder favicon

--- resources/views/layouts/builder.blade.php
+++ resources/views/layouts/builder.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Finder Editor' }}</title>
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @fluxAppearance
 </head>
