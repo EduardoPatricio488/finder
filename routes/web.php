7061fcca2725b6cd1624371375bd640e60c0c08e
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-17 15:39:32 +0100

    Remove Builder route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\AdminAssistant;
 use App\Livewire\AdminDashboard;
-use App\Livewire\BuilderEditor;
 use App\Livewire\CategoryManager;
 use App\Livewire\CreateSite;
 use App\Livewire\CustomerAccount;
@@ -50,7 +49,6 @@ Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('dashboard', UserDashboard::class)->name('dashboard');
     Route::get('ajuda', Help::class)->name('help');
     Route::get('websites/create', CreateSite::class)->name('site.create');
-    Route::get('websites/{site:slug}/builder', BuilderEditor::class)->name('builder.edit');
     Route::get('conta/{section?}', CustomerAccount::class)->name('account');
     Route::get('conta/favoritos', fn () => redirect()->route('account', ['section' => 'favorites']))->name('account.favorites');
     Route::get('encomendas/acompanhamento', OrderTracking::class)->name('orders.tracking');
