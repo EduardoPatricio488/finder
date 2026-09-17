4280015441cec9571f3ea0227ab407098375e0e3
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-17 15:36:27 +0100

    Rebuild builder as simple site preview

--- resources/views/livewire/builder-editor.blade.php
+++ resources/views/livewire/builder-editor.blade.php
@@ -1,1 +1,8 @@
-<div class="min-h-screen bg-white"></div>
+<div class="h-screen w-screen overflow-hidden bg-white">
+    <iframe
+        src="{{ route('site.public', ['site' => $site, 'preview' => 1]) }}"
+        title="Pré-visualização do website {{ $site->name }}"
+        class="block h-full w-full border-0"
+        loading="eager"
+    ></iframe>
+</div>
