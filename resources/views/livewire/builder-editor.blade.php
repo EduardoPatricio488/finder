17943ece95a1c8b7cc1199de38fa4f465172d36d
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-17 15:39:37 +0100

    Delete Builder view

--- resources/views/livewire/builder-editor.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="h-screen w-screen overflow-hidden bg-white">
-    <iframe
-        src="{{ route('site.public', $site) }}?preview=1"
-        title="Pré-visualização do website {{ $site->name }}"
-        class="block h-full w-full border-0"
-        loading="eager"
-    ></iframe>
-</div>
