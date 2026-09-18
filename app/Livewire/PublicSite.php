c987539cea88f9992db653ba2e5a0589a61b49bf
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-18 12:49:16 +0100

    Fix public site Livewire root after media integration

--- app/Livewire/PublicSite.php
+++ app/Livewire/PublicSite.php
@@ -130,7 +130,7 @@ class PublicSite extends Component
     public function render(): mixed
     {
         $view = $this->site->type === 'personal'
-            ? 'livewire.personal-site-renderer-with-media'
+            ? 'livewire.personal-site-renderer'
             : 'livewire.public-site-renderer';
 
         $media = $this->site->media
