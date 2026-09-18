026bbad1cc344327d76d4dc592df703e8bbc277e
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-18 11:24:17 +0100

    Restore UpgradeSelection Livewire page

---
+++ app/Livewire/UpgradeSelection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Plan;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+#[Layout('layouts.hub')]
+class UpgradeSelection extends Component
+{
+    public function selectPlan($planId)
+    {
+        $user = auth()->user();
+        $plan = Plan::findOrFail($planId);
+        $user->update(['plan' => $plan->name]);
+
+        session()->flash('status', 'Upgrade concluído! Já podes criar o teu site.');
+
+        return redirect()->route('home');
+    }
+
+    public function render()
+    {
+        return view('livewire.upgrade-selection', [
+            'plans' => Plan::orderBy('price_monthly', 'asc')->get(),
+        ]);
+    }
+}
