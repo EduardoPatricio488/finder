a093b7af55b3627388240522e3eca1ea8e522692
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-18 12:12:27 +0100

    Store media placement

--- app/Models/SiteMedia.php
+++ app/Models/SiteMedia.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['site_id', 'uploaded_by', 'disk', 'path', 'original_name', 'mime_type', 'size', 'alt_text', 'width', 'height'])]
+#[Fillable(['site_id', 'uploaded_by', 'disk', 'path', 'original_name', 'mime_type', 'size', 'alt_text', 'placement', 'width', 'height'])]
 class SiteMedia extends Model
 {
     public function site(): BelongsTo
