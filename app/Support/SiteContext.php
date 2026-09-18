6b205edb1c26beb6964dbaa5abfec88bd5d29170
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-18 18:53:53 +0100

    Concluir migração para URLs globais

--- app/Support/SiteContext.php
+++ app/Support/SiteContext.php
@@ -52,7 +52,7 @@ class SiteContext
 
     public static function adminRoute(string $name, ?Site $site = null): string
     {
-        return route('admin.site.'.$name);
+        return route('site.manage.'.$name);
     }
 
     public static function manageableSitesQuery(User $user): Builder
